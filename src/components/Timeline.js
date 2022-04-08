8543c330cec4d73d9b41713a202e1910cb714f88
jacob jemal <jacobjemal7@gmail.com>
2022-04-08 13:25:29 -0400

    addee post to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,16 +3,28 @@ import { useEffect, useState } from 'react';
 const Timeline = () => {
     const [posts, setPosts] = useState([])
 
-    useEffect(async () => {
-        console.log('Rendering Timeline ...')
+    useEffect(() => {
+        // IIFE: immediately invoked function expression
+        (async () => {
         let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
         let res = await req.json()
         setPosts(res)
+        })()
     }, [])
 
     return(
         <div>
             <h2>Timeline</h2>
+            {
+                posts.map((element) => {
+                    return(
+                        <div> 
+                            <p>{element.username}</p>
+                            <p>{element.content}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
