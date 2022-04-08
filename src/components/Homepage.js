0d5a917137572681bc8020e41fbe8bc8f67a9877
jacob jemal <jacobjemal7@gmail.com>
2022-04-08 14:19:03 -0400

    updaated title

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,6 @@ import Sidebar from "./Sidebar";
 const Homepage = ({  }) => {
     return(
         <div>
-            <h2>Welcome to life</h2>
             <Timeline /> 
             <Sidebar />
         </div>
